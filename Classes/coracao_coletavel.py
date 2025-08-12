da33c34d3d178249591f8293750b0de2552da096
Gabriel Mezzalira <gabrielmezzalira@MacBook-Pro-de-Gabriel.local>
2025-08-11 23:06:03 -0300

    adicionamos o slow final e o coracao_coletavel, além de outras mudanças.

--- Classes/coracao_coletavel.py
+++ Classes/coracao_coletavel.py
@@ -7,13 +7,10 @@ class Coracao:
         self._surf = pg.image.load(f"Imagens/{arquivo}.png")
         self._surf = pg.transform.scale(self._surf, (44, 48))
 
-
         pos_y_aleatoria = random.randint(200, 500)
         self._rect = self._surf.get_rect(topleft=(-100, pos_y_aleatoria))
         self.hitbox = self._rect.inflate(-15, -17)
     
-
-
     def mover(self, velocidade):
         self._rect.x += velocidade
         self.hitbox.centerx = self._rect.centerx
